c84f244e8aeed0dd6fe151deac92cee790d5f221
Manh Cuong <cuong@radioactive.sg>
2023-01-27 16:16:57 +0800

    update slider

--- Assets/Scripts/CollectionScene.cs
+++ Assets/Scripts/CollectionScene.cs
@@ -15,42 +15,44 @@ public class CollectionScene : MonoBehaviour
     {
         Time.timeScale = 1;
         string lastSavedGhosts = PlayerPrefs.GetString(Constant.PLAYER_PREFS_CATCH_TIME);
-        if (lastSavedGhosts != null)
+        if (lastSavedGhosts == null)
         {
-            string[] ghosts = lastSavedGhosts.Split(
-                new string[] { Constant.PLAYER_PREFS_SEPERATOR },
-                System.StringSplitOptions.None
-            );
-            Dictionary<string, string> ghostKey = new Dictionary<string, string>();
-            for (int i = 0; i + 1 < ghosts.Length; i += 2)
+            lastSavedGhosts =
+                "1" + Constant.PLAYER_PREFS_SEPERATOR + "XXXXXX" + Constant.PLAYER_PREFS_SEPERATOR;
+        }
+        string[] ghosts = lastSavedGhosts.Split(
+            new string[] { Constant.PLAYER_PREFS_SEPERATOR },
+            System.StringSplitOptions.None
+        );
+        Dictionary<string, string> ghostKey = new Dictionary<string, string>();
+        for (int i = 0; i + 1 < ghosts.Length; i += 2)
+        {
+            if (!ghostKey.ContainsKey(ghosts[i]))
             {
-                if (!ghostKey.ContainsKey(ghosts[i]))
-                {
-                    ghostKey.Add(ghosts[i], ghosts[i + 1]);
-                    GameObject ghost = Object.Instantiate(GhostInfor, Content.transform);
-                    Level levelInfor = ConfigManager.Instance.GetLevelInfo(int.Parse(ghosts[i]));
+                ghostKey.Add(ghosts[i], ghosts[i + 1]);
+                GameObject ghost = Object.Instantiate(GhostInfor, Content.transform);
+                Level levelInfor = ConfigManager.Instance.GetLevelInfo(int.Parse(ghosts[i]));
 
-                    myTexture = Resources.Load("Images/" + levelInfor.image) as Texture2D;
-                    Transform rawImage = ghost.transform.Find("[Image]GhostImage");
-                    string ghostDescription =
-                        levelInfor.ghostName
-                        + "\n"
-                        + "Catched Time: "
-                        + ghosts[i + 1]
-                        + "\n"
-                        + levelInfor.description;
-                    rawImage.GetComponent<RawImage>().texture = myTexture;
-                    ghost.transform.Find("[Text]Description").GetComponentInChildren<TMP_Text>().text =
-                        ghostDescription;
-                    /*ghost.transform
-                        .Find("[Text]CatchTime")
-                        .GetComponentInChildren<TMP_Text>()
-                        .text = "Catched Time: " + ghosts[i + 1];
-                    ghost.transform
-                        .Find("[Text]Description")
-                        .GetComponentInChildren<TMP_Text>()
-                        .text = levelInfor.ghostName;*/
-                }
+                myTexture = Resources.Load("Images/" + levelInfor.image) as Texture2D;
+                Transform rawImage = ghost.transform.Find("[Image]GhostImage");
+                string ghostDescription =
+                    levelInfor.ghostName
+                    + "\n"
+                    + "Catched Time: "
+                    + ghosts[i + 1]
+                    + "\n"
+                    + levelInfor.description;
+                rawImage.GetComponent<RawImage>().texture = myTexture;
+                ghost.transform.Find("[Text]Description").GetComponentInChildren<TMP_Text>().text =
+                    ghostDescription;
+                /*ghost.transform
+                    .Find("[Text]CatchTime")
+                    .GetComponentInChildren<TMP_Text>()
+                    .text = "Catched Time: " + ghosts[i + 1];
+                ghost.transform
+                    .Find("[Text]Description")
+                    .GetComponentInChildren<TMP_Text>()
+                    .text = levelInfor.ghostName;*/
             }
         }
     }
